ed7e61eb4de6c55a8e256baf54068f1e35eff1ef
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-10-12 14:01:58 +0200

    Add test code to the repository.

---
+++ test/t.erl
@@ -0,0 +1,45 @@
+-module(t).
+
+-include_lib("amqp_client/include/amqp_client.hrl").
+
+-export([t_start/0, t/0]).
+
+-spec f(binary()) -> {reply, binary()} | ack | reject | reject_no_requeue.
+f(<<"Hello">>) ->
+  {reply, <<"ok">>}.
+
+t_start() ->
+	application:start(amqp_client),
+	%% Spawn a RabbitMQ server system:
+	ConnInfo = #amqp_params_network { username = <<"guest">>, password = <<"guest">>,
+	                                  host = "localhost", port = 5672 },
+	{ok, SPid} = amqp_server_sup:start_link(
+	    server_connection_mgr, ConnInfo, <<"test_queue">>,
+	                                     [{<<"x-message-ttl">>, long, 30000},
+	                                      {<<"x-dead-letter-exchange">>, longstr, <<"dead-letters">>}], fun f/1, 5),
+	{ok, CPid} = amqp_client_sup:start_link(
+	    client_connection, client_connection_mgr, 
+	    ConnInfo, <<"test_queue">>),
+	{ok, SPid, CPid}.
+
+t() ->
+    Parent = self(),
+	Pids = [spawn_link(fun () -> do_work(Parent, 1000) end) || _ <- lists:seq(1, 100)],
+	collect(Pids).
+	
+collect([]) ->
+  done;
+collect(Pids) ->
+  receive
+    {done, Pid} -> collect(Pids -- [Pid])
+  end.
+
+do_work(Parent, N) ->
+    do_work_(N),
+    Parent ! {done, self()}.
+
+do_work_(0) -> ok;
+do_work_(N) ->
+	amqp_rpc_client2:call(client_connection, <<"Hello">>),
+	do_work_(N-1).
+
