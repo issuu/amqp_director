aea52c7397e0e1d9dec4e64157bf478e04f90352
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-10-17 15:59:09 +0200

    Add support for the client to be configurable. Note yet tested.

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+REPO=amqp_director
+
 all: deps compile-deps
 
 deps:
@@ -11,3 +13,32 @@ compile:
 
 clean:
 	rebar clean
+
+APPS = kernel stdlib sasl erts ssl tools os_mon runtime_tools crypto inets \
+	xmerl webtool snmp public_key mnesia eunit syntax_tools compiler
+COMBO_PLT = $(HOME)/.$(REPO)_combo_dialyzer_plt
+
+check_plt: compile
+	dialyzer --check_plt --plt $(COMBO_PLT) --apps $(APPS) \
+		deps/*/ebin
+
+build_plt: compile
+	dialyzer --build_plt --output_plt $(COMBO_PLT) --apps $(APPS) \
+		deps/amqp_client/ebin\
+		deps/rabbit_common/ebin
+
+dialyzer:
+	@echo
+	@echo Use "'make check_plt'" to check PLT prior to using this target.
+	@echo Use "'make build_plt'" to build PLT prior to using this target.
+	@echo
+	@sleep 1
+	dialyzer -Wno_return --plt $(COMBO_PLT) ebin #| grep -F -v -f ./dialyzer.ignore-warnings
+
+cleanplt:
+	@echo
+	@echo "Are you sure?  It takes about 1/2 hour to re-build."
+	@echo Deleting $(COMBO_PLT) in 5 seconds.
+	@echo
+	sleep 5
+	rm $(COMBO_PLT)
--- src/amqp_rpc_client2.erl
+++ src/amqp_rpc_client2.erl
@@ -31,12 +31,13 @@
 -behaviour(gen_server).
 
 -export([start_link/3]).
--export([cast/2, call/2, call/3]).
+-export([cast/4, call/3, call/4]).
 -export([init/1, terminate/2, code_change/3, handle_call/3,
          handle_cast/2, handle_info/2]).
 
 -record(state, {channel,
                 reply_queue,
+                app_id,
                 exchange,
                 routing_key,
                 continuations = dict:new(),
@@ -49,79 +50,115 @@
 %% @doc Starts a new RPC client instance that sends requests to a
 %% specified queue. This function returns the pid of the RPC client process
 %% that can be used to invoke RPCs and stop the client.
--spec start_link(Name, ConnRef, RoutingKey) -> {ok, pid()}
+-spec start_link(Name, Configuration, ConnRef) -> {ok, pid()}
   when Name :: atom(),
-       ConnRef :: pid(),
-       RoutingKey :: binary().
-start_link(Name, ConnRef, RoutingKey) ->
-    gen_server:start_link({local, Name}, ?MODULE, [ConnRef, RoutingKey], []).
+       Configuration :: term(),
+       ConnRef :: pid().
+start_link(Name, Configuration, ConnRef) ->
+    gen_server:start_link({local, Name}, ?MODULE, [ConnRef, Configuration], []).
 
 %% @doc Send a fire-and-forget message to the queue.
 %% This implements the usual cast operation where a message is forwarded to a queue.
 %% Note that there is *no* guarantee that the message will be sent. In particular,
-%% if the queue is down, the message will be lost.
+%% if the queue is down, the message will be lost. You also have to supply a ContentType
+%% as well as a message type for the system.
 %% @end
--spec cast(RpcClient, Payload) -> ok
+-spec cast(RpcClient, Payload, ContentType, Type) -> ok
   when RpcClient :: atom() | pid(),
-       Payload :: binary().
-cast(RpcClient, Payload) ->
-  gen_server:cast(RpcClient, {cast, Payload}).
+       Payload :: binary(),
+       ContentType :: binary(),
+       Type :: binary().
+cast(RpcClient, Payload, ContentType, Type) ->
+  gen_server:cast(RpcClient, {cast, Payload, ContentType, Type}).
 
-%% @equiv call(RpcClient, Payload, 5000)
-call(RpcClient, Payload) ->
-    call(RpcClient, Payload, 5000).
+%% @equiv call(RpcClient, Payload, ContentType, 5000)
+call(RpcClient, Payload, ContentType) ->
+    call(RpcClient, Payload, ContentType, 5000).
 
 %% @doc Invokes an RPC. Note the caller of this function is responsible for
 %% encoding the request and decoding the response. If the timeout is hit, the
-%% calling process will exit.
+%% calling process will exit. The call will set `ContentType' as the type of
+%% the message (essentially the mime type). The `Type' of the message will always
+%% be set to `request'.
 %% @end
--spec call(RpcClient, Request, Timeout) -> Response
+-spec call(RpcClient, Request, ContentType, Timeout) -> Response
   when RpcClient :: atom() | pid(),
        Request :: binary(),
+       ContentType :: binary(),
        Timeout :: pos_integer(),
        Response :: {ok, binary()} | {error, term()}.
-call(RpcClient, Payload, Timeout) ->
-    gen_server:call(RpcClient, {call, Payload}, Timeout).
+call(RpcClient, Payload, ContentType, Timeout) ->
+    gen_server:call(RpcClient, {call, Payload, ContentType}, Timeout).
 
 %%--------------------------------------------------------------------------
 
+%% Initialize a set of queues.
+queue_initialize(_Channel, []) -> ok;
+queue_initialize(Channel, [#'queue.declare' { queue = Q} = QDec | Defns]) ->
+    #'queue.declare_ok' { queue = Q } = amqp_channel:call(Channel, QDec),
+    queue_initialize(Channel, Defns);
+queue_initialize(Channel, [#'queue.bind' {} = BindDec | Defns]) ->
+    #'queue.bind_ok' {} = amqp_channel:call(Channel, BindDec),
+    queue_initialize(Channel, Defns).
+
 %% Sets up a reply queue for this client to listen on
-setup_reply_queue(State = #state{channel = Channel}) ->
-    #'queue.declare_ok'{queue = Q} =
-        amqp_channel:call(Channel, #'queue.declare'{}),
-    State#state{reply_queue = Q}.
+setup_queues(State = #state{channel = Channel}, Configuration) ->
+    queue_initialize(Channel, proplists:get_value(queue_definitions, Configuration, [])),
+
+    %% Configuration of the Reply queue:
+    case proplists:get_value(reply_queue, Configuration) of
+      undefined ->
+          % Set up an no-name reply queue
+          #'queue.declare_ok'{queue = ReplyQ} = amqp_channel:call(Channel, #'queue.declare'{});
+      none ->
+          % No reply queue wanted, do not set up one. Calls won't work.
+          ReplyQ = none;
+      ReplyQ ->
+          #'queue.declare_ok' { queue = ReplyQ } = amqp_channel:call(Channel, #'queue.declare'{ queue = ReplyQ })
+    end,
+    State#state{reply_queue = ReplyQ}.
 
 %% Registers this RPC client instance as a consumer to handle rpc responses
+setup_consumer(#state{channel = _Channel, reply_queue = none}) ->
+    ok;
 setup_consumer(#state{channel = Channel, reply_queue = Q}) ->
     amqp_channel:call(Channel, #'basic.consume'{queue = Q}).
 
 %% Publishes to the broker, stores the From address against
 %% the correlation id and increments the correlationid for
 %% the next request
-publish(Payload, From,
+publish(Payload, ContentType, From,
         State = #state{channel = Channel,
                        reply_queue = Q,
                        exchange = X,
                        routing_key = RoutingKey,
                        correlation_id = CorrelationId,
+                       app_id = AppId,
                        continuations = Continuations}) ->
+    %% Properties should follow the rules of:
+    %% http://trac.tissuu.com:8000/trac/wiki/IssuuAmqp
     Props = #'P_basic'{correlation_id = <<CorrelationId:64>>,
-                       content_type = <<"application/octet-stream">>,
+                       content_type = ContentType,
+                       type = <<"request">>,
+                       app_id = AppId,
                        reply_to = Q},
     Publish = #'basic.publish'{exchange = X,
                                routing_key = RoutingKey,
                                mandatory = true},
-    amqp_channel:call(Channel, Publish, #amqp_msg{props = Props,
-                                                  payload = Payload}),
+    amqp_channel:call(Channel, Publish,
+                      #amqp_msg{props = Props,
+                                payload = Payload}),
     State#state{correlation_id = CorrelationId + 1,
                 continuations = dict:store(CorrelationId, From, Continuations)}.
 
 %% Publish on a queue in a fire-n-forget fashion.
-publish_cast(Payload,
+publish_cast(Payload, ContentType, Type,
              #state { channel = Channel,
                       exchange = X,
                       routing_key = RoutingKey }) ->
-    Props = #'P_basic' { content_type = <<"application/octet-stream">> },
+    Props = #'P_basic'{content_type = ContentType,
+                       type = Type,
+                       app_id = list_to_binary(atom_to_list(node()))},
     Publish = #'basic.publish'{exchange = X,
                                routing_key = RoutingKey,
                                mandatory = true},
@@ -132,8 +169,8 @@ publish_cast(Payload,
 
 %% Sets up a reply queue and consumer within an existing channel
 %% @private
-init([ConnectionRef, RoutingKey]) ->
-	{ok, try_connect(ConnectionRef, RoutingKey, 1000)}.
+init([ConnectionRef, Configuration]) ->
+	{ok, try_connect(ConnectionRef, Configuration, 1000)}.
 	
 
 %% Closes the channel this gen_server instance started
@@ -148,25 +185,28 @@ handle_call(stop, _From, State) ->
     {stop, normal, ok, State};
 
 %% @private
-handle_call({call, _Payload}, _From, #state { channel = undefined } = State) ->
+handle_call({call, _Payload, _ContentType}, _From, #state { channel = undefined } = State) ->
     {reply, {error, no_connection}, State};
-handle_call({call, Payload}, From, State) ->
-    NewState = publish(Payload, From, State),
+handle_call({call, _, _}, _From, #state { reply_queue = none } = State) ->
+    {reply, {error, no_call_configuration}, State};
+handle_call({call, Payload, ContentType}, From, State) ->
+    NewState = publish(Payload, ContentType, From, State),
     {noreply, NewState}.
 
 %% @private
-handle_cast({cast, _Payload}, #state { channel = undefined } = State) ->
+handle_cast({cast, _Payload, _ContentType, _Type}, #state { channel = undefined } = State) ->
     %% We can't do anything but throw away the message here!
+    error_logger:info_msg("Warning - throwing away message for an undefined channel."),
     {noreply, State};
-handle_cast({cast, Payload}, State) ->
-    publish_cast(Payload, State),
+handle_cast({cast, Payload, ContentType, Type}, State) ->
+    publish_cast(Payload, ContentType, Type, State),
     {noreply, State};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
 %% @private
-handle_info({reconnect, CRef, RoutingKey, ReconnectTime}, #state { channel = undefined }) ->
-    {noreply, try_connect(CRef, RoutingKey, ReconnectTime)};
+handle_info({reconnect, CRef, Configuration, ReconnectTime}, #state { channel = undefined }) ->
+    {noreply, try_connect(CRef, Configuration, ReconnectTime)};
 handle_info({'DOWN', _, process, Channel, Reason},
             #state { channel = Channel } = State) ->
     error_logger:info_msg("Channel ~p going down... stopping", [Channel]),
@@ -194,20 +234,23 @@ code_change(_OldVsn, State, _Extra) ->
 
 %%--------------------------------------------------------------------------
     
-try_connect(ConnectionRef, RoutingKey, ReconnectTime) ->
+try_connect(ConnectionRef, Configuration, ReconnectTime) ->
 	case amqp_connection_mgr:fetch(ConnectionRef) of
 	    {ok, Connection} ->
 	      {ok, Channel} = amqp_connection:open_channel(Connection, {amqp_direct_consumer, [self()]}),
           erlang:monitor(process, Channel),
           InitialState = #state{channel     = Channel,
-                                exchange    = <<>>,
-                                routing_key = RoutingKey},
-          State = setup_reply_queue(InitialState),
+                                exchange    = proplists:get_value(exchange, Configuration, <<>>),
+                                app_id      = proplists:get_value(app_id, Configuration,
+                                                                  list_to_binary(atom_to_list(node()))),
+                                routing_key = proplists:get_value(routing_key, Configuration)},
+          State = setup_queues(InitialState, Configuration),
           setup_consumer(State),
           State;
         {error, econnrefused} ->
           error_logger:info_msg("RPC Client has no working channel, waiting"),
-          timer:send_after(ReconnectTime, self(), {reconnect, ConnectionRef, RoutingKey,
+          timer:send_after(ReconnectTime, self(), {reconnect, ConnectionRef, Configuration,
                                                               min(ReconnectTime * 2, ?MAX_RECONNECT)}),
           #state { channel = undefined }
       end.
+
