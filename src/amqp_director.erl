9025d3e15a75722062c547d2c404f6f871f10e96
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-11-15 11:00:32 +0100

    Fix compilation error.

--- src/amqp_director.erl
+++ src/amqp_director.erl
@@ -27,7 +27,7 @@ mk_app_id(RegName) when is_atom(RegName) ->
   {Mega, S, _} = os:timestamp(),
   iolist_to_binary(
     [Hostname, $., atom_to_list(node()), $.,
-     integer_to_list(Creation), $.
+     integer_to_list(Creation), $.,
      integer_to_list(Mega * 1000000 + S), atom_to_list(RegName)]).
 
 -spec add_connection( atom(), #amqp_params_network{} ) ->
