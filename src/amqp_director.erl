0a6e3f4477c85860122c96653c8a7e7cc68b8682
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-11-23 16:04:30 +0100

    Kill unused code.

--- src/amqp_director.erl
+++ src/amqp_director.erl
@@ -2,9 +2,8 @@
 
 -include_lib("amqp_client/include/amqp_client.hrl").
 
--export ([children_specs/2]).
--export ([parse_connection_parameters/1]).
--export ([child_spec/1, server_child_spec/5, client_child_spec/3]).
+-export([parse_connection_parameters/1]).
+-export([server_child_spec/5, client_child_spec/3]).
 -export([mk_app_id/1]).
 
 %% @doc Construct an application Id for this node based on a RegName atom
@@ -41,87 +40,3 @@ parse_connection_parameters(Props) ->
     _Otherwise ->
       exit({error, parse_connection_parameters})
   end.
-
--type children_type() :: servers | clients | all.
--spec children_specs(atom(), children_type()) -> [ supervisor:child_spec() ].
-children_specs(App, Type) when is_atom(App) ->
-    {ok, Config} = application:get_env(App, amqp_director),
-    ConnectionsConf = proplists:get_value(connections, Config), % will fail if no connection is defined
-    ComponentsConf = proplists:get_value(components, Config),   % will fail if no component is defined
-
-    Connections = lists:map(fun ({Name, Overrides}) ->
-        {Name, setup_amqp_record({amqp_params_network, Overrides})}
-    end, ConnectionsConf),
-
-    [ child_spec( prepare_conf(ChildConf, Connections) )
-      || ChildConf <- ComponentsConf, is_component_type(ChildConf, Type) ].
-
-
--type child_conf() :: {atom(), {atom(), atom()}, #amqp_params_network{}, integer(), [{atom(), term()}]}
-                    | {atom(), #amqp_params_network{}, [{atom(), term()}]}.
--spec child_spec( child_conf() ) -> supervisor:child_spec().
-child_spec( {Name, Fun, ConnInfo, ServersCount, Config} ) ->
-    server_child_spec(Name, Fun, ConnInfo, ServersCount, Config);
-child_spec( {Name, ConnInfo, Config} ) ->
-    client_child_spec(Name, ConnInfo, Config).
-
-%%%
-%%% Internals
-%%%
-is_component_type( _Any, all ) -> true;
-is_component_type( {_Name, _ModFun, _ConnRef, _Count, _Conf}, servers ) -> true;
-is_component_type( {_Name, _ConnRef, _Conf}, clients ) -> true;
-is_component_type( _Component, _Type ) -> false.
-
-make_fun(Mod, Fun) ->
-    fun( Request, ContentType, MessageType ) ->
-        apply( Mod, Fun, [Request, ContentType, MessageType] )
-    end.
-
-prepare_conf({Name, {Mod,Fun}, ConnRef, Count, Config}, Connections) ->
-    {ConnRef, ConnInfo} = lists:keyfind(ConnRef, 1, Connections),
-    {Name, make_fun(Mod, Fun), ConnInfo, Count, config(Config)};
-prepare_conf({Name, ConnRef, Config}, Connections) ->
-    {ConnRef, ConnInfo} = lists:keyfind(ConnRef, 1, Connections),
-    {Name, ConnInfo, config(Config)}.
-
-config(Config) ->
-    [ {K, setup_config(K, V)} || {K, V} <- Config ].
-
-setup_config(queue_definitions, Records) ->
-    [ setup_amqp_record(Record) || Record <- Records ];
-setup_config(_K, V) -> V. % we should't need to touch other things
-
-setup_amqp_record( {amqp_params_network, KV} ) ->
-    Fields = record_info(fields, amqp_params_network),
-    create_record(Fields, #amqp_params_network{}, KV);
-setup_amqp_record( {'queue.declare', KV} ) ->
-    Fields = record_info(fields, 'queue.declare'),
-    create_record(Fields, #'queue.declare'{}, KV);
-setup_amqp_record( {'queue.bind', KV} ) ->
-    Fields = record_info(fields, 'queue.bind'),
-    create_record(Fields, #'queue.bind'{}, KV).
-
-create_record(Fields, Empty, KVs) ->
-    % If the template contains fields not mentioned in the record definition, let us fail
-    case lists:filter(fun ({Key, _}) -> not(lists:member(Key, Fields))  end, KVs) of
-        [] -> ok;
-        Some -> exit({bad_record_template, lists:nth(1, Fields), Some})
-    end, 
-    % zip field names and empty record
-    FieldsValues = lists:zip( [record_name]++Fields, tuple_to_list(Empty) ),
-    % map the zipped list to the new values
-    NewValues = [ proplists:get_value(Key, KVs, Value) || {Key, Value} <- FieldsValues ],
-    % return the newly constructed record
-    list_to_tuple(NewValues).
-
-
-dependent_apps() ->
-    {ok, Apps} = application:get_key(amqp_director, applications),
-    Apps -- [kernel, stdlib].
-
-ensure_started(App) ->
-    case application:start(App) of
-        ok -> ok;
-        {error,{already_started, App}} -> ok
-    end.
