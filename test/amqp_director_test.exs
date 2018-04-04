9d2a8378a0eae456a87c5155d5378872f56f7fbf
Pawel Antemijczuk <pan@issuu.com>
2018-04-04 13:41:01 +0200

    Test that works

--- test/amqp_director_test.exs
+++ test/amqp_director_test.exs
@@ -2,6 +2,10 @@ defmodule AmqpDirectorTest do
   use ExUnit.Case
   doctest AmqpDirector
 
+  @amqp_host "localhost"
+  @amqp_username "guest"
+  @amqp_password "guest"
+
   test "Creating RPC server spec" do
     spec = AmqpDirector.server_child_spec(
       :test_name,
@@ -15,6 +19,33 @@ defmodule AmqpDirectorTest do
 
     assert {:test_name, {:amqp_server_sup, :start_link, _},
           :permanent, :infinity, :supervisor, [:amqp_server_sup]} = spec
-
   end
+
+  test "Client/server pattern" do
+    serverSpec = AmqpDirector.server_child_spec(
+      :test_name,
+      &handler/3,
+      [host: @amqp_host, username: @amqp_username, password: @amqp_password],
+      2,
+      [queue_definitions: [
+        AmqpDirector.exchange_declare("test_exchange"),
+        AmqpDirector.queue_declare("test_queue", auto_delete: true),
+        AmqpDirector.queue_bind("test_queue", "test_exchange", "test_key")
+      ],
+      consume_queue: "test_queue"
+      ]
+    )
+    clientSpec = AmqpDirector.client_child_spec(
+      :test_client,
+      [host: @amqp_host, username: @amqp_username, password: @amqp_password],
+      []
+    )
+    Supervisor.start_link([serverSpec, clientSpec], strategy: :one_for_one)
+
+    AmqpDirector.Client.await(:test_client)
+    {:ok, "reply", "application/x-erlang-term"} = AmqpDirector.Client.call(:test_client, "test_exchange", "test_key", "some_msg", "application/x-erlang-term", [])
+  end
+
+  defp handler("some_msg", "application/x-erlang-term", "request"), do: {:reply, "reply", "application/x-erlang-term"}
+  defp handler(_, _, _), do: {:reply, "wrong_msg", "application/x-erlang-term"}
 end
